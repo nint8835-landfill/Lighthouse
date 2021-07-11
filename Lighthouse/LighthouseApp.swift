b980b56e9cfd87ee0f8abc0ed8acb443fb4fdf63
Riley Flynn <riley@rileyflynn.me>
2021-07-11 00:31:47 -0230

    Initial Commit

---
+++ Lighthouse/LighthouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LighthouseApp.swift
+//  Lighthouse
+//
+//  Created by Riley Flynn on 2021-07-11.
+//
+
+import SwiftUI
+
+@main
+struct LighthouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
